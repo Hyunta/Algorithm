1d733f8d680b3bde3092ce673714428f883e893c
Hyunta <mohai2618@naver.com>
2021-09-12 17:10:55 +0900

    210912 알고리즘 풀이

--- Test/월간 코드 챌린지 시즌3 9월/1.py
+++
@@ -1,8 +0,0 @@
-def solution(numbers):
-    tmp = [i for i in range(1,10)]
-    for number in numbers:
-        while number in tmp:
-            tmp.remove(number)
-    return sum(tmp)
-
-print(solution([1,2,3,4,6,7,8,0]))
