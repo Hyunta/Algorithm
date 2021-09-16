33a090ff3d85e3a46a7efdb83f81c0452d6c2eba
Hyunta <mohai2618@naver.com>
2021-09-16 14:35:32 +0900

    210915 알고리즘 풀이

--- Baekjoon/1932.py
+++ Baekjoon/1932.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
@@ -6,13 +7,13 @@ t = []
 for i in range(n):
     t.append(list(map(int, input().split())))
 k = 2
-for i in range(1,n):
+for i in range(1, n):
     for j in range(k):
         if j == 0:
-            t[i][j] = t[i][j] + t[i-1][j]
+            t[i][j] = t[i][j] + t[i - 1][j]
         elif j == i:
-            t[i][j] = t[i][j] +t[i-1][j-1]
+            t[i][j] = t[i][j] + t[i - 1][j - 1]
         else:
-            t[i][j] = max(t[i-1][j-1],t[i-1][j]) + t[i][j]
+            t[i][j] = max(t[i - 1][j - 1], t[i - 1][j]) + t[i][j]
     k += 1
-print(t)
+print(max(t[n - 1]))
