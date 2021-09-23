716b3294c8790c7c77c9bb0a6a330b4fe4d1c645
Hyunta <mohai2618@naver.com>
2021-09-23 17:04:44 +0900

    210923 알고리즘 풀이

---
+++ Baekjoon/9466.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+
+def dfs(x):
+    global result
+    visited[x] = True
+    cycle.append(x)
+    number = numbers[x]
+
+    if visited[number]:
+        if number in cycle:
+            result += cycle[cycle.index(number):]
+        return
+    else:
+        dfs(number)
+
+
+for _ in range(int(input())):
+    n = int(input())
+    numbers = [0] + list(map(int, input().split()))
+    visited = [True] + [False] * n
+    result = []
+
+    for i in range(1,n+1):
+        if not visited[i]:
+            cycle = []
+            dfs(i)
+    print(n - len(result))
+
+
