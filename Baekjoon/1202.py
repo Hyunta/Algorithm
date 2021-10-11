dd53b24db4bc2b824ba4dd6cba8a2138a9001389
Hyunta <mohai2618@naver.com>
2021-10-11 19:12:33 +0900

    211011 알고리즘 풀이

---
+++ Baekjoon/1202.py
@@ -0,0 +1,25 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+jewels = [list(map(int, input().split())) for _ in range(n)]
+bags = [int(input()) for _ in range(k)]
+jewels.sort()
+bags.sort()
+
+res = 0
+tmp =[]
+
+for bag in bags:
+    while jewels and bag >= jewels[0][0]:
+        heapq.heappush(tmp, -jewels[0][1])
+        heapq.heappop(jewels)
+
+    if tmp:
+        res += heapq.heappop(tmp)
+    elif not jewels:
+        break
+print(-res)
