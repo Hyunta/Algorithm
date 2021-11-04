c4a14ac9dba0f2dc047710933979413dcbf083f9
Hyunta <mohai2618@naver.com>
2021-11-04 16:07:54 +0900

    211104 알고리즘 풀이

---
+++ Baekjoon/16953.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+a, b = map(int, input().split())
+
+res = -1
+q = deque()
+q.append([a, 1])
+while q:
+    i, cnt = q.popleft()
+    if i == b:
+        res = cnt
+        break
+
+    if i * 2 <= b:
+        q.append([i * 2, cnt + 1])
+    if int(str(i) + '1') <= b:
+        q.append([int(str(i) + '1'), cnt + 1])
+print(res)
