798ad699cb5908645a7d0027f6ef1f51b509ec79
Hyunta <mohai2618@naver.com>
2021-11-10 11:53:06 +0900

    211110 알고리즘 풀이

--- Programmers/우테/7.py
+++ Programmers/우테/7.py
@@ -2,7 +2,7 @@ def solution(grid, clockwise):
     if clockwise:
         new_grid = [""] * len(grid)
         for i in range(len(grid)):
-            
+            pass
 
 
 print(solution(["1", "234", "56789"], True))
