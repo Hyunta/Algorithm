67bc3bee26018f4fee0501962071d00556ff3aff
Hyunta <mohai2618@naver.com>
2021-11-15 10:09:03 +0900

    211115 알고리즘 풀이

--- Baekjoon/~2021.06.07/2167.py
+++ Baekjoon/~2021.06.07/2167.py
@@ -1,16 +1,15 @@
 import sys
+
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-arr = list()
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-k = int(input())
-for _ in range(k):
-    tmp_r = 0
-    i,j,x,y = map(int, input().split())
-    for a in range(i-1,x):
-        for b in range(j-1, y):
-            tmp_r += arr[a][b]
-    print(tmp_r)
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = graph[i - 1][j - 1] + dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1]
+
+for _ in range(int(input())):
+    i, j, x, y = map(int, input().split())
+    print(dp[x][y] - dp[x][j - 1] - dp[i - 1][y] + dp[i - 1][j - 1])
