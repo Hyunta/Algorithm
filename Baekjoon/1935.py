c9c9de6852813f841f40643c27c80804dd8f4bca
Hyunta <mohai2618@naver.com>
2021-11-16 10:14:53 +0900

    211116 알고리즘 풀이

---
+++ Baekjoon/1935.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+tmp = input().rstrip()
+stack = []
+nums = [int(input()) for _ in range(n)]
+
+def calculate(first, second, operator):
+    if operator == '+':
+        return first + second
+    elif operator == '-':
+        return first - second
+    elif operator == '*':
+        return first * second
+    elif operator == '/':
+        return first / second
+
+
+for x in tmp:
+    if x.isalpha():
+        stack.append(nums[ord(x) - ord('A')])
+
+    else:
+        b = float(stack.pop())
+        a = float(stack.pop())
+        stack.append(calculate(a, b, x))
+print(f'{stack[0]:.2f}')
