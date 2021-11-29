8077b8ab87995d151b5a262fc71642b1a0898bbc
Hyunta <mohai2618@naver.com>
2021-11-29 10:43:26 +0900

    211129 알고리즘 풀이

--- Baekjoon/9465.py
+++ Baekjoon/9465.py
@@ -5,11 +5,13 @@ t = int(input())
 for _ in range(t):
     n = int(input().rstrip())
     stickers = [list(map(int, input().split())) for _ in range(2)]
-    stickers[0][1] += stickers[1][0]
-    stickers[1][1] += stickers[0][0]
-    for i in range(2, n):
-        stickers[0][i] += max(stickers[1][i - 1], stickers[1][i - 2])
-        stickers[1][i] += max(stickers[0][i - 1], stickers[0][i - 2])
+    for i in range(1, n):
+        if i == 1:
+            stickers[0][1] += stickers[1][0]
+            stickers[1][1] += stickers[0][0]
+        else:
+            stickers[0][i] += max(stickers[1][i - 1], stickers[1][i - 2])
+            stickers[1][i] += max(stickers[0][i - 1], stickers[0][i - 2])
     answer = 0
     for sticker in stickers:
         answer = max(answer, max(sticker))
