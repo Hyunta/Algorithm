f0e274317ce70667db2457c56147e66b6c48fe74
Hyunta <mohai2618@gmail.com>
2021-12-20 10:49:36 +0900

    211220 알고리즘 풀이

---
+++ Baekjoon/16926.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+
+def rotate_board(r):
+    global N, M, board
+    q = deque()
+    _width, _height = M, N
+    time = min(_width, _height) // 2
+    nx, ny = 0, 0
+
+    while time >= 1:
+        for i in range(_width - 1):
+            q.append(board[ny][nx + i])
+        for i in range(_height - 1):
+            q.append(board[ny + i][nx + _width - 1])
+        for i in range(_width - 1):
+            q.append(board[ny + _height - 1][nx + _width - 1 - i])
+        for i in range(_height - 1):
+            q.append(board[ny + _height - 1 - i][nx])
+
+        q.rotate(-r)
+
+        for i in range(_width - 1):
+            board[ny][nx + i] = q.popleft()
+        for i in range(_height - 1):
+            board[ny + i][nx + _width - 1] = q.popleft()
+        for i in range(_width - 1):
+            board[ny + _height - 1][nx + _width - 1 - i] = q.popleft()
+        for i in range(_height - 1):
+            board[ny + _height - 1 - i][nx] = q.popleft()
+
+        _width -= 2
+        _height -= 2
+        nx += 1
+        ny += 1
+        time = min(_width, _height) // 2
+
+
+input = sys.stdin.readline
+N, M, rotate_count = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+rotate_board(rotate_count)
+for i in range(N):
+    print(*board[i])
