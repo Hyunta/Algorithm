879a9105138e2d4fab49a23a87bee52a01225ecf
Hyunta <mohai2618@naver.com>
2022-01-29 22:40:32 +0900

    220129 알고리즘 풀이

---
+++ Baekjoon/4101.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    n, m = map(int, input().split())
+    if (n, m) == (0, 0):
+        break
+    if n > m:
+        print("Yes")
+    else:
+        print("No")
