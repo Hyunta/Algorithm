e6a4e9273112eb581680ac9a29b42a3d8b4976f7
Hyunta <mohai2618@naver.com>
2022-02-02 02:36:29 +0900

    20210202 알고리즘 풀이

---
+++ Baekjoon/2501.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+div = []
+for i in range(1, n + 1):
+    if n % i == 0:
+        div.append(i)
+if k > len(div):
+    print(0)
+else:
+    print(div[k - 1])
